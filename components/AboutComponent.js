1f4e0ab04bc41717d48efe32226322d92bb488a7
billynguyenlss <billynguyen.lss@gmail.com>
2020-03-17 08:54:29 +0700

    activity indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,14 +1,25 @@
 import React, {Component} from 'react';
-import {View, Text, FlatList} from 'react-native';
+import {ScrollView, Text, FlatList} from 'react-native';
 import {Card, ListItem} from 'react-native-elements';
 import {LEADERS} from '../shared/leaders';
 import {connect} from 'react-redux';
 import {baseUrl} from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
         leaders: state.leaders
     }
+}
+
+const History = () => {
+    return (
+        <Card title='History'>
+            <Text>Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.
+                <br></br><br></br>
+            The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.</Text>
+        </Card>
+    )
 }
 
 class About extends Component {
@@ -30,24 +41,50 @@ class About extends Component {
                     hideChevron={true}
                     leftAvatar={{ source: {uri: baseUrl + item.image}}}
                 />
+            );
+        }
+
+        if (this.props.leaders.isLoading) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                            <Loading />
+                    </Card>
+
+                </ScrollView>
             )
         }
-        return(
-            <View>
-                <Card title='History'>
-                    <Text>Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.
-                        <br></br><br></br>
-                    The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.</Text>
-                </Card>
-                <Card title='Corporate Leadership'>
-                    <FlatList 
-                        data={this.props.leaders.leaders}
-                        renderItem={renderLeader}
-                        keyExtractor={item => item.id.toString()}
-                    />
-                </Card>
-            </View>
-        );
+        else if (this.props.leaders.errMess) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                            <Text>
+                                {this.props.leaders.errMess}
+                            </Text>
+                    </Card>
+
+                </ScrollView>
+            )
+        }
+        else {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card title='Corporate Leadership'>
+                        <FlatList 
+                            data={this.props.leaders.leaders}
+                            renderItem={renderLeader}
+                            keyExtractor={item => item.id.toString()}
+                        />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        
     }
 
 }
--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -8,6 +8,7 @@ import {DISHES} from '../shared/dishes';
 import {connect} from 'react-redux';
 import {baseUrl} from '../shared/baseUrl';
 import { StackActions } from '@react-navigation/native';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -19,21 +20,36 @@ const mapStateToProps = state => {
 
 function RenderItem(props){
     const item = props.item;
-    if (item != null){
+    if (props.isLoading) {
         return(
-            
-            <Card featuredTitle={item.name}
-                featuredSubtitle={item.designation}
-                image={{uri: baseUrl + item.image}}>
-                <Text style={{margin:10}}>{item.description}</Text>
-            </Card>
+            <Loading />
         );
-    } else {
+    } 
+    else if (props.errMess) {
         return(
-            <View></View>
-        );
+            <View>
+                <Text>
+                    {props.errMess}
+                </Text>
+            </View>
+        )
     }
-    
+    else {
+        if (item != null){
+            return(
+                
+                <Card featuredTitle={item.name}
+                    featuredSubtitle={item.designation}
+                    image={{uri: baseUrl + item.image}}>
+                    <Text style={{margin:10}}>{item.description}</Text>
+                </Card>
+            );
+        } else {
+            return(
+                <View></View>
+            );
+        }
+    }    
 }
 
 class Home extends React.Component {
@@ -50,9 +66,21 @@ class Home extends React.Component {
     render(){
         return(
             <ScrollView>
-                <RenderItem item={this.props.dishes.dishes.filter((dish) => dish.featured)[0]} />
-                <RenderItem item={this.props.promotions.promotions.filter((promo) => promo.featured)[0]} />
-                <RenderItem item={this.props.leaders.leaders.filter((leader) => leader.featured)[0]} />
+                <RenderItem 
+                    item={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+                    isLoading={this.props.dishes.isLoading}
+                    errMess={this.props.dishes.errMess}    
+                />
+                <RenderItem 
+                    item={this.props.promotions.promotions.filter((promo) => promo.featured)[0]} 
+                    isLoading={this.props.promotions.isLoading}
+                    errMess={this.props.promotions.errMess}    
+                />
+                <RenderItem 
+                    item={this.props.leaders.leaders.filter((leader) => leader.featured)[0]} 
+                    isLoading={this.props.leaders.isLoading}
+                    errMess={this.props.leaders.errMess}
+                />
             </ScrollView>
         )
     }
